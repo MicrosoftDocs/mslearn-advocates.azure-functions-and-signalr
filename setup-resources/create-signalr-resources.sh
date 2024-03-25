51ed907525d5f6c1b47580e77e2ce8c3797c68ea
Dina Berry (MSFT) <diberry@microsoft.com>
2024-03-25 21:03:26 +0000

    Setup files

---
+++ setup-resources/create-signalr-resources.sh
@@ -0,0 +1,48 @@
+# To use the Microsoft Learn Sandbox in the training module
+# https://learn.microsoft.com/training/modules/automatic-update-of-a-webapp-using-azure-functions-and-signalr
+# To run: sign in to Azure CLI with `az login`
+set -e
+
+NODE_ENV_FILE="./.env"
+
+SUBSCRIPTION_NAME="Concierge Subscription"
+
+az account set --subscription "$SUBSCRIPTION_NAME"
+echo "User default subscription set to $SUBSCRIPTION_NAME"
+
+RESOURCE_GROUP_NAME=$(az group list --query '[0].name' -o tsv)
+echo "Using resource group $RESOURCE_GROUP_NAME"
+
+SIGNALR_SERVICE_NAME="msl-sigr-signalr$(openssl rand -hex 5)"
+
+echo "Subscription Name: $SUBSCRIPTION_NAME"
+echo "Resource Group Name: $RESOURCE_GROUP_NAME"
+echo "SignalR Service Name: $SIGNALR_SERVICE_NAME"
+
+echo "Creating SignalR Account"
+az signalr create \
+    --name $SIGNALR_SERVICE_NAME \
+    --resource-group $RESOURCE_GROUP_NAME \
+    --sku Free_DS2 \
+    --unit-count 1
+
+echo "Configure SignalR for serverless mode"
+az resource update \
+  --resource-type Microsoft.SignalRService/SignalR \
+  --name $SIGNALR_SERVICE_NAME \
+  --resource-group $RESOURCE_GROUP_NAME \
+  --set properties.features[flag=ServiceMode].value=Serverless
+
+  echo "Get SignalR connection string"
+
+  SIGNALR_CONNECTION_STRING=$(az signalr key list \
+  --name $(az signalr list \
+    --resource-group $RESOURCE_GROUP_NAME \
+    --query [0].name -o tsv) \
+  --resource-group $RESOURCE_GROUP_NAME \
+  --query primaryConnectionString -o tsv)
+
+echo "SignalR Connection String: $SIGNALR_CONNECTION_STRING"
+
+# Add the SignalR connection string to the .env file
+echo -e "\nSIGNALR_CONNECTION_STRING=$SIGNALR_CONNECTION_STRING" >> $NODE_ENV_FILE
