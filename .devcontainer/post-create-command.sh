9b16ed16e360ef4c62725582abed7817f0499fc9
Dina Berry (MSFT) <diberry@microsoft.com>
2024-03-26 21:26:49 +0000

    install the dependencies

--- .devcontainer/post-create-command.sh
+++ .devcontainer/post-create-command.sh
@@ -1,1 +1,7 @@
 npm install -g azure-functions-core-tools@4.5098 --save-exact --unsafe-perm true
+
+cd setup-resources && npm install && cd ..
+cd client-start && npm install && cd ..
+cd server-start && npm install && cd ..
+cd client-end && npm install && cd ..
+cd server-end && npm install && cd ..
