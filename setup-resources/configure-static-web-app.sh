8e062f2c84b7a95d336281d76d1ec1853e4ca866
Dina Berry (personal) <dfberry@users.noreply.github.com>
2024-03-28 14:12:03 +0000

    update

--- setup-resources/configure-static-web-app.sh
+++ setup-resources/configure-static-web-app.sh
@@ -15,16 +15,6 @@ echo "Getting the connection string for the Cosmos DB account..."
 cosmosDbConnectionString=$(az cosmosdb keys list --name $cosmosDbAccount --resource-group $resourceGroup --type connection-strings --query 'connectionStrings[0].connectionString' -o tsv)
 echo "Cosmos DB Connection String: $cosmosDbConnectionString"
 
-# Get the first Static Web App in the resource group
-echo "Getting the first Static Web App in the resource group..."
-staticWebAppName=$(az staticwebapp list --resource-group $resourceGroup --query '[0].name' -o tsv)
-echo "Static Web App Name: $staticWebAppName"
-
-# Add the environment variable to the Static Web App
-echo "Adding the environment variable to the Static Web App..."
-az staticwebapp appsettings set --name $staticWebAppName --resource-group $resourceGroup --settings COSMOS_CONNECTION_STRING=$cosmosDbConnectionString
-echo "Environment variable added."
-
 # Get the first SignalR service in the resource group
 echo "Getting the first SignalR service in the resource group..."
 signalrName=$(az signalr list --resource-group $resourceGroup --query '[0].name' -o tsv)
@@ -35,7 +25,12 @@ echo "Getting the connection string for the SignalR service..."
 signalrConnectionString=$(az signalr key list --name $signalrName --resource-group $resourceGroup --query 'primaryConnectionString' -o tsv)
 echo "SignalR Connection String: $signalrConnectionString"
 
+# Get the first Static Web App in the resource group
+echo "Getting the first Static Web App in the resource group..."
+staticWebAppName=$(az staticwebapp list --resource-group $resourceGroup --query '[0].name' -o tsv)
+echo "Static Web App Name: $staticWebAppName"
+
 # Add the environment variable to the Static Web App
-echo "Adding the SignalR connection string environment variable to the Static Web App..."
-az staticwebapp appsettings set --name $staticWebAppName --resource-group $resourceGroup --settings SIGNALR_CONNECTION_STRING=$signalrConnectionString
-echo "SignalR connection string environment variable added."
+echo "Adding the environment variables to the Static Web App..."
+az staticwebapp appsettings set --name $staticWebAppName --resource-group $resourceGroup --setting-names SIGNALR_CONNECTION_STRING=$signalrConnectionString COSMOSDB_CONNECTION_STRING=$cosmosDbConnectionString
+echo "Set environment variables."
