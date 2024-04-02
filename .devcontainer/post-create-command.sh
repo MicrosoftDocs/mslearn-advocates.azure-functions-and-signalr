b023cfddab2f576f77cf961f81b5860d8ebd06a0
Dina Berry <dinaberry@outlook.com>
2024-04-02 19:36:57 +0000

    updates

--- .devcontainer/post-create-command.sh
+++ .devcontainer/post-create-command.sh
@@ -1,4 +1,4 @@
-npm install -g azure-functions-core-tools@4.5098 --save-exact --unsafe-perm true
+npm install -g azure-functions-core-tools@4.5611 --unsafe-perm true
 
 cd setup-resources && npm install && cd ..
 cd client-start && npm install && cd ..
