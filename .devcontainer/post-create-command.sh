cd85891d005d7e53876255687c9973699e832f3a
Dina Berry (personal) <dfberry@users.noreply.github.com>
2024-04-03 14:54:14 -0700

    Final fixes (#78)

--- .devcontainer/post-create-command.sh
+++ .devcontainer/post-create-command.sh
@@ -1,7 +1,3 @@
-npm install -g azure-functions-core-tools@4.5098 --save-exact --unsafe-perm true
+npm install -g azure-functions-core-tools@4.5611 --unsafe-perm true
 
 cd setup-resources && npm install && cd ..
-cd client-start && npm install && cd ..
-cd server-start && npm install && cd ..
-cd client-end && npm install && cd ..
-cd server-end && npm install && cd ..
