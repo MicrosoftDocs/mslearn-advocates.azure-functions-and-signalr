f5a12528896324834de29c242f69978d2757932d
amirnajmi.nl <amirnajmi@Amirs-MBP.home>
2024-09-24 14:51:08 +0200

    add some new configs

--- start/client/src/index.js
+++ start/client/src/index.js
@@ -1,44 +1,60 @@
 import './style.css';
-
-function getApiUrl() {
-
-    const backend = process.env.BACKEND_URL;
-    
-    const url = (backend) ? `${backend}` : ``;
-    return url;
-}
+import { BACKEND_URL } from './env';
 
 const app = new Vue({
     el: '#app',
-    interval: null,
-    data() { 
+    data() {
         return {
             stocks: []
         }
     },
     methods: {
-        async update() {
+        async getStocks() {
             try {
-                
-                const url = `${getApiUrl()}/api/getStocks`;
+
+                const url = `${BACKEND_URL}/api/getStocks`;
                 console.log('Fetching stocks from ', url);
 
                 const response = await fetch(url);
                 if (!response.ok) {
-                    throw new Error(`HTTP error! status: ${response.status}`);
+                    throw new Error(`HTTP error! status: ${response.status} - ${response.statusText}`);
                 }
-
                 app.stocks = await response.json();
             } catch (ex) {
                 console.error(ex);
             }
-        },
-        startPoll() {
-            this.interval = setInterval(this.update, 5000);
         }
     },
     created() {
-        this.update();
-        this.startPoll();
+        this.getStocks();
     }
 });
+
+const connect = () => {
+
+    const signalR_URL = `${BACKEND_URL}/api`;
+    console.log(`Connecting to SignalR...${signalR_URL}`)
+
+    const connection = new signalR.HubConnectionBuilder()
+                            .withUrl(signalR_URL)
+                            .configureLogging(signalR.LogLevel.Information)
+                            .build();
+
+    connection.onclose(()  => {
+        console.log('SignalR connection disconnected');
+        setTimeout(() => connect(), 2000);
+    });
+
+    connection.on('updated', updatedStock => {
+        console.log('Stock updated message received', updatedStock);
+        const index = app.stocks.findIndex(s => s.id === updatedStock.id);
+        console.log(`${updatedStock.symbol} Old price: ${app.stocks[index].price}, New price: ${updatedStock.price}`);
+        app.stocks.splice(index, 1, updatedStock);
+    });
+
+    connection.start().then(() => {
+        console.log("SignalR connection established");
+    });
+};
+
+connect();
